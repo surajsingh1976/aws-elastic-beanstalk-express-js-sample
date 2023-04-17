0a5ee916ddf72aab358564aaab5d7c6a68551ce6
surajsingh1976 <74875307+surajsingh1976@users.noreply.github.com>
2023-04-18 01:39:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('May the force be with you'));
+app.get('/', (req, res) => res.send('Terminated Beanstalk'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
